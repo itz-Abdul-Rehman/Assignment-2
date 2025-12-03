a67745403ba066cad79825fcf248f9a6572213bf
itz-Abdul-Rehman <abrehmanmughal1234@gmail.com>
2025-12-02 20:34:16 -0800

    Update Array Reverse.cpp

--- Array Reverse.cpp
+++ Array Reverse.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 using namespace std;
-void reverseArr(int &arr[], int n)
+
+void reverseArr(int *arr, int n)
 {
+    int a, b;
+    for (int i = 0; i < n; i++)
+    {
+        if ((n - (i + 1)) - i >= 0)
+        {
+            a = arr[i];
+            b = arr[n - (i + 1)];
+            a = a * b; // a=2*3=6
+            b = a / b; // b=6/3=2
+            a = a / b; // a=6/2=3
+            arr[i] = a;
+            arr[n - (i + 1)] = b;
+        }
+        else
+        {
+            break;
+        }
+    }
+    cout << "Reversed Array: \n";
     for (int j = 0; j < n; j++)
     {
         cout << arr[j] << " ";
@@ -18,10 +38,12 @@ int main()
     {
         cin >> arr[i];
     }
+    cout << "Actual Array";
     for (int j = 0; j < n; j++)
     {
         cout << arr[j] << " ";
     }
+    cout << endl;
     reverseArr(arr, n);
     return 0;
 }
